44b40675ccab7f7abec99501a5e6e31477885ec5
ankitklu <anit.klu.2022@gmail.com>
2025-12-13 19:43:23 +0530

    Primary Constructor

--- src/Main.kt
+++ src/Main.kt
@@ -17,13 +17,32 @@ class Person2(var name: String, var age:Int){
 
 }
 
+//Primary constructor and Initializer Blocks
+class Person3(fName: String, personAge: Int){
+    val firstName: String;
+    var age: Int = 12;
+
+    init{
+
+        firstName = fName.capitalize()
+        age = personAge
+        println("First Name = $firstName")
+        println("Age = $age")
+    }
+
+}
+
 fun main(){
     var person = Person()
-    person.setName("Ankit");
-    println(person.getName());
+//    person.setName("Ankit");
+//    println(person.getName());
+//
+//    var person2 = Person2("Ankit", 18);
+//    println(person2.canVote());
 
-    var person2 = Person2("Ankit", 18);
-    println(person2.canVote());
+    var person3 = Person3("ankit", 21);
+
+
 }
 fun test(){
     println("Hello Test ")
---
+++ src/readme.md
@@ -0,0 +1,24 @@
+# Types of Constructors
+- Primary Constructor : concise way to initialize a class
+- Secondary constructor : allows you to put additional initialization logic
+
+## Primary Constructor
+```java
+class Person2(var name: String, var age:Int){
+    fun canVote(): Boolean{
+        return if (age>18) true else false;
+    }
+
+}
+```
+The primary constructor is part of the class header.
+To put the initialization code , initializer vkicj us s=used to prefixed with init keyword . Let;s modufy the above example with initializer block.
+
+```java
+
+```
+
+## Secondary Constructor
+```java
+
+```
