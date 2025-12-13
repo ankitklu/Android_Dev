54af536f2057257b3748d906c1bdc63885e052e1
ankitklu <anit.klu.2022@gmail.com>
2025-12-13 19:09:25 +0530

    Inheritance: parameterless | with parameters

--- src/Main.kt
+++ src/Main.kt
@@ -9,10 +9,21 @@ class Person{
     }
 }
 
+// class with parameters
+class Person2(var name: String, var age:Int){
+    fun canVote(): Boolean{
+        return if (age>18) true else false;
+    }
+
+}
+
 fun main(){
     var person = Person()
     person.setName("Ankit");
     println(person.getName());
+
+    var person2 = Person2("Ankit", 18);
+    println(person2.canVote());
 }
 fun test(){
     println("Hello Test ")
