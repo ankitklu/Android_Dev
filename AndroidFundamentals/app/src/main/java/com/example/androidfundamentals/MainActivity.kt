c03dd2a74a387a1473779b5426418ae390f207f9
ankitklu <ankit.klu.2022@gmail.com>
2025-12-15 14:50:51 +0530

    Android : Android Activity lifecycle logs

---
+++ AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.androidfundamentals
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidfundamentals.ui.theme.AndroidFundamentalsTheme
+
+class MainActivity : ComponentActivity() {
+    private val TAG = "MainActivityLifecycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate")
+        enableEdgeToEdge()
+        setContent {
+            AndroidFundamentalsTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "..onStart..")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidFundamentalsTheme {
+        Greeting("Android")
+    }
+}
