1adda88b2c8c8b863502771ae76cf937ea24c3b2
ankitklu <ankit.klu.2022@gmail.com>
2025-12-22 14:27:19 +0530

    Android : Style/Themes | Dimensions | Strings

---
+++ Strings_themes_color_dimensions/app/src/main/java/com/example/strings_themes_color_dimensions/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.strings_themes_color_dimensions
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.strings_themes_color_dimensions.ui.theme.Strings_themes_color_dimensionsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Strings_themes_color_dimensionsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Strings_themes_color_dimensionsTheme {
+        Greeting("Android")
+    }
+}
