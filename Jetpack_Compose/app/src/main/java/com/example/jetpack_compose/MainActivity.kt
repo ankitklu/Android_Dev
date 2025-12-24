39b6f48a0e6abce2886f673c976369952960aef1
ankitklu <ankit.klu.2022@gmail.com>
2025-12-24 11:56:08 +0530

    Android Compose : onClick | mutableStateof | remember

--- Jetpack_Compose/app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ Jetpack_Compose/app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -16,10 +16,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,60 +40,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Jetpack_ComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Ankit",
-                        modifier = Modifier.padding(innerPadding)
+
+                val count = remember {
+                    mutableStateOf(0)
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = count.value.toString(),
+                        fontSize = 30.sp
                     )
+                    Button(onClick={
+                        count.value++
+                    }) {
+                        Text(text = "Click Me")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-//    Box (
-//        modifier = Modifier.size(400.dp)
-//    ) {
-//        Text(
-//            text = "Hello $name !",
-//            color = Color.Blue,
-//            fontSize = 30.sp,
-//            modifier = Modifier.align(Alignment.BottomEnd)
-//        )
-//        Text(
-//            text = "Some other",
-//            color = Color.Blue,
-//            fontSize = 30.sp,
-//        )
-//    }
-//
-//    Image(
-//        painter = painterResource(R.drawable.ic_launcher_foreground),
-//        contentDescription = null,
-//        modifier = Modifier.background(Color.Black)
-//    )
-//
-//    if(name.length == 4){
-//        Icon(imageVector = Icons.Default.Add, contentDescription = null)
-//    }
-    LazyColumn(modifier = Modifier.fillMaxSize()){
-        items(10)
-        {
-            i-> Icon(imageVector = Icons.Default.Add, contentDescription = null, modifier = Modifier.size(100.dp))
-        }
-
-    }
-
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Jetpack_ComposeTheme {
-        Greeting("Ankit")
     }
 }
