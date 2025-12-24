f7e9c22d1f08acf3829eb64b12910b13757d3dc1
ankitklu <ankit.klu.2022@gmail.com>
2025-12-24 14:26:34 +0530

    Android Compose : Simple Add to list app with handleclicks| mutableListOf and mutableStateOf

--- Jetpack_Compose/app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ Jetpack_Compose/app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -1,29 +1,41 @@
 package com.example.jetpack_compose
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,26 +53,59 @@ class MainActivity : ComponentActivity() {
         setContent {
             Jetpack_ComposeTheme {
 
-                val count = remember {
-                    mutableStateOf(0)
-                }
+                var name by remember { mutableStateOf("") }
+                var names by remember { mutableStateOf(listOf<String>()) }
 
                 Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier.fillMaxSize().padding(16.dp)
                 ) {
-                    Text(
-                        text = count.value.toString(),
-                        fontSize = 30.sp
-                    )
-                    Button(onClick={
-                        count.value++
-                    }) {
-                        Text(text = "Click Me")
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { name = it },
+                            modifier = Modifier.weight(1f),
+                            label = { Text("Enter name") }
+                        )
+
+                        Spacer(modifier= Modifier.width(16.dp).height(16.dp))
+                        Button(
+                            onClick = {
+                                if(name.isNotBlank()){
+                                    names = names + name
+                                    name = ""
+                                }
+                            },
+                            modifier = Modifier.padding(start = 8.dp)
+                        ) {
+                            Text("Add")
+                        }
+                    }
+
+                    LazyColumn(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        items(names) { currentName ->
+                            Text(
+                                text = currentName,
+                                fontSize = 20.sp,
+                                modifier = Modifier.padding(4.dp).padding(5.dp)
+                            )
+                            HorizontalDivider(
+                                color = Color.Gray,
+                                thickness = 1.dp
+                            )
+                        }
                     }
                 }
             }
         }
+
     }
 }
