d5586eb6135c0721fcf9462b0e71b172fdfb0f07
ankitklu <ankit.klu.2022@gmail.com>
2025-12-27 14:44:36 +0530

    Android : Navigation3 | NavHost | onNavigate | popBackStack()

--- Nav3_Practice/app/src/main/java/com/example/nav3_practice/MainActivity.kt
+++ Nav3_Practice/app/src/main/java/com/example/nav3_practice/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.nav3_practice.ui.theme.Nav3_PracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Nav3_PracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNavigation()
         }
     }
 }
@@ -37,6 +36,51 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+@Composable
+fun HomeScreen(onNavigate: () -> Unit) {
+    Button(onClick = onNavigate) {
+        Text("Go to Details")
+    }
+}
+
+@Composable
+fun DetailScreen(navController: NavHostController) {
+    Column {
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Back")
+        }
+        Text("This is the Detail Screen")
+    }
+}
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+
+        composable("home") {
+            HomeScreen(
+                onNavigate = {
+                    navController.navigate("details")
+                }
+            )
+        }
+
+        composable("details") {
+            DetailScreen(navController)
+        }
+
+    }
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
